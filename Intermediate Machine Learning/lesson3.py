10c06d71e536886fd6cef272fc87ff3841a37c84
hangwl <hangwei54e@hotmail.com>
2022-07-02 02:53:23 +0800

    lesson 3

---
+++ Intermediate Machine Learning/lesson3.py
@@ -0,0 +1,11 @@
+# Categorical Variables
+
+# 3 basic approaches:
+
+# 1. Drop categorical variables
+# 2. Ordinal Encoding - assign each unique value to a different integer
+# 3. One-Hot Encoding - create new columns indicating the presence (or absence) of each possible value in the original data
+
+# In general, approach #3 will typically perform best, and approach #1 the worst, but it varies on a case-by-case basis
+
+
